558d7fc20901514af15df90b9c53654d36c6a7ff
anna <annarschneider@gmail.com>
2015-07-26 21:42:35 -0700

    add AA to corpus

--- parse_corpus.sh
+++ parse_corpus.sh
@@ -1,10 +1,13 @@
-./parse_corpus_file.py corpus_raw/apple.txt corpus/apple.txt Apple apple.com
+mkdir corpus
+
+./parse_corpus_file.py corpus_raw/american_air.txt corpus/american_air.txt AAdvantage aa.com 'American Airlines'
+./parse_corpus_file.py corpus_raw/apple.txt corpus/apple.txt Apple apple.com 'Apple Inc.'
 ./parse_corpus_file.py corpus_raw/chrome.txt corpus/chrome.txt Chrome google.com Google
-./parse_corpus_file.py corpus_raw/dropbox.txt corpus/dropbox.txt Dropbox dropbox.com
-./parse_corpus_file.py corpus_raw/facebook.txt corpus/facebook.txt Facebook facebook.com
+./parse_corpus_file.py corpus_raw/dropbox.txt corpus/dropbox.txt Dropbox dropbox.com 'Dropbox, Inc'
+./parse_corpus_file.py corpus_raw/facebook.txt corpus/facebook.txt Facebook facebook.com Facebook
 ./parse_corpus_file.py corpus_raw/hoefler.txt corpus/hoefler.txt Hoefler typography.com 'H&Co'
 ./parse_corpus_file.py corpus_raw/league_of_legends.txt corpus/league_of_legends.txt 'League of Legends' leagueoflegends.com 'Riot Games'
-./parse_corpus_file.py corpus_raw/minecraft.txt corpus/minecraft.txt Minecraft minecraft.com
-./parse_corpus_file.py corpus_raw/netflix.txt corpus/netflix.txt Netflix netflix.com
+./parse_corpus_file.py corpus_raw/minecraft.txt corpus/minecraft.txt 'Our Game' minecraft.com Minecraft
+./parse_corpus_file.py corpus_raw/netflix.txt corpus/netflix.txt Netflix netflix.com 'Netflix, Inc.'
 ./parse_corpus_file.py corpus_raw/sublime.txt corpus/sublime.txt 'Sublime Text' sublime.com 'SUBLIME HQ'
 ./parse_corpus_file.py corpus_raw/world_of_warcraft.txt corpus/world_of_warcraft.txt 'World of Warcraft' blizzard.com 'Blizzard Entertainment'
